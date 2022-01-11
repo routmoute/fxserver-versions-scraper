334aad722e6fa6c2e4a2a6250107813b34ba8605
Routmoute <tom@yourlabs.org>
2022-01-11 23:05:47 +0100

    ci: update
    fix(ci): temporary trigger dev branch
    
    ci: fix
    
    patch(scrape): add github action outputs
    
    ci: add scrape outputs to trigger
    
    ci: test
    
    Revert "ci: test"
    
    This reverts commit 9194da22433874e0d421dbcec81e065d617d3edd.
    
    ci: manual run
    
    ci: fix
    
    ci: fix
    
    ci: test
    
    ci: test
    
    ci
    
    ci: test
    
    [Auto] New FXServer versions
    
    ci: test
    
    ci: test
    
    ci: test
    
    ci: test
    
    ci: fix
    
    ci: test
    
    ci: test
    
    ci: fix
    
    ci: fix

--- scrape.js
+++ scrape.js
@@ -5,10 +5,10 @@ import cheerio from 'cheerio';
 const versionsFilePath = process.env.VERSIONS_FILE_PATH;
 const artifactsUrl = process.env.ARTIFACTS_URL;
 if (!versionsFilePath) {
-    throw new Error("VERSIONS_FILE_PATH env var not set !");
+  throw new Error("VERSIONS_FILE_PATH env var not set !");
 }
 if (!artifactsUrl) {
-    throw new Error("ARTIFACTS_URL env var not set !");
+  throw new Error("ARTIFACTS_URL env var not set !");
 }
 
 let versions = JSON.parse(fs.readFileSync(versionsFilePath));
@@ -44,17 +44,20 @@ const latest = {
 let newVersion = false
 
 if (versions.recommended.version != recommended.version) {
-  console.log('New recommended version detected ! - ' + versions.recommended.version + ' => ' + recommended.version)
+  console.log('New recommended version detected ! - ' + versions.recommended.version + ' => ' + recommended.version);
+  console.log('::set-output name=recommended::' + recommended.version);
   versions.recommended = recommended;
   newVersion = true;
 }
 if (versions.optional.version != optional.version) {
-  console.log('New optional version detected ! - ' + versions.optional.version + ' => ' + optional.version)
+  console.log('New optional version detected ! - ' + versions.optional.version + ' => ' + optional.version);
+  console.log('::set-output name=optional::' + optional.version);
   versions.optional = optional;
   newVersion = true;
 }
 if (versions.latest.version != latest.version) {
-  console.log('New latest version detected ! - ' + versions.latest.version + ' => ' + latest.version)
+  console.log('New latest version detected ! - ' + versions.latest.version + ' => ' + latest.version);
+  console.log('::set-output name=latest::' + latest.version);
   versions.latest = latest;
   newVersion = true;
 }
